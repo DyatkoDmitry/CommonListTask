939343a4c26594aade532fe3e16f141ffe052d21
Dyatko Dmitry <DyatkoDmitry@gmail.com>
2024-03-24 09:20:20 +0300

    feat: init: initial commit

---
+++ app/src/main/java/com/example/commonlisttask/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.commonlisttask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
